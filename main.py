4dbcce012e543c6dab2a66d5f9a4fc1f8db7e0fd
OofChair <oofchairr@gmail.com>
2021-02-25 20:43:44 -0500

    Initial commit, make public :eyes:

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 from typing import Optional
 import sr_api
 from fastapi import FastAPI
-
+__version__ = "0.0.1"
 api = FastAPI()
 
 client = sr_api.Client()
 
 @api.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-
-@api.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    return {"Welcome to": "OofAPI", "Made with": "FastAPI", "FastAPI": "https://fastapi.tiangolo.com", "Copyright 2021": "OofChair https://oofchair.xyz", "Version": __version__}
 
 @api.get("/dog")
 async def dog_fact():
