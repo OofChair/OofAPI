9e49952a672389272475039b0c0473cb0958997b
OofChair <oofchairr@gmail.com>
2021-02-25 20:58:06 -0500

    Initial commit, version 0.0.1, make public, add license, etc.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # PwnAPI
+
+This API was created as a fun side project by [OofChair](https://github.com/OofChair). <br><br> 
+### This is **NOT FINISHED** and may not be for a while.
+
+#### This was created using [FastAPI](https://fastapi.tiangolo.com) and uses the [sr_api](https://pypi.org/project/sr_api) pip package.
+
+#### Want to run this yourself?
+
+##### Easy, make a new venv. (On Linux)
+`python -m venv ~/venv` <br>
+`source ~/venv/bin/activate`<br>
+`pip install fastapi uvicorn[standard] sr_api`<br>
+`cd source`<br>
+`uvicorn main:api` <br>
+##### To run it so your changes happen in real time, run `uvicorn main:api --reload`.
--- source/main.py
+++ source/main.py
@@ -1,14 +1,23 @@
 from typing import Optional
 import sr_api
 from fastapi import FastAPI
+
 __version__ = "0.0.1"
 api = FastAPI()
 
 client = sr_api.Client()
 
+
 @api.get("/")
 def read_root():
-    return {"Welcome to": "OofAPI", "Made with": "FastAPI", "FastAPI": "https://fastapi.tiangolo.com", "Copyright 2021": "OofChair https://oofchair.xyz", "Version": __version__}
+    return {
+        "Welcome to": "OofAPI",
+        "Made with": "FastAPI",
+        "FastAPI": "https://fastapi.tiangolo.com",
+        "Copyright 2021": "OofChair https://oofchair.xyz",
+        "Version": __version__,
+    }
+
 
 @api.get("/dog")
 async def dog_fact():
