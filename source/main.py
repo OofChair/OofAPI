20870c21e22a59394b984965251f225b6500f742
OofChair <oofchairr@gmail.com>
2021-02-28 10:57:50 -0500

    bad commit message here :D

--- source/main.py
+++ source/main.py
@@ -1,4 +1,3 @@
-from typing import Optional
 import sr_api
 from fastapi import FastAPI
 
@@ -16,10 +15,11 @@ def read_root():
         "FastAPI": "https://fastapi.tiangolo.com",
         "Copyright 2021": "OofChair https://oofchair.xyz",
         "Version": __version__,
+        "Home page": "Coming soon!",
     }
 
 
-@api.get("/fact/dog")
+@api.get("/api/fact/dog")
 async def dog_fact():
     fact = await client.get_fact("dog")
     return {"fact": fact}
